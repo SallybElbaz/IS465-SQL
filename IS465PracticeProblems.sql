8ec16ff1d2bd8e6bdfb057f6becae2972a8fd1b5
SallybElbaz <92899228+SallybElbaz@users.noreply.github.com>
2021-10-21 18:58:41 -0400

    Update IS465PracticeProblems.sql

--- IS465PracticeProblems.sql
+++ IS465PracticeProblems.sql
@@ -78,4 +78,117 @@ where c.customernumber = p.customernumber and amount > 100000
 order by amount desc;
 /*List all products ordered by customer Herkku Gifts, sorted by order date.
 Display productName, quantityOrdered, priceEach, orderDate*/
+select p.productname,od.quantityOrdered,od.priceEach, o.orderDate from 
+products p, orderdetails od, orders o where od.productCode = p.productCode 
+and od.ordernumber = o.ordernumber and
+o.customernumber = (select customernumber from customers 
+						 where customerName = 'Herkku Gifts')
+order by o.orderdate;
+/*Without using a join, get a distinct list of employees who represent customers*/						 
+ select concat(firstname,' ',lastname) as EmployeeName from employees where 
+ employeenumber in (select distinct salesrepemployeenumber from customers); 
+ /*Find the customer who made the highest payment and report their customer ID*/
+select customernumber from payments where amount =(select Max(amount)from payments); 
+/*Find the customer who made the highest payment, but this time 
+project all payment fields, and order payments by amount from highest to lowest.
+*/
+select * from payments where customernumber in(
+select customernumber from payments where amount =(select Max(amount)from payments)
+);
+/*Find the number of customers that each employee represents. 
+The results of your query should be 
+(employee number, number of customers that the employee represents).*/
+select c.salesrepemployeenumber,count(*) from customers c
+group by c.salesrepemployeenumber;
+/*How many payments have been made by each customer? 
+Show the customer name and the number of payments that customer has made.*/
+select c.customername,count(*) numPayments from customers c, payments p
+where c.customernumber = p.customernumber
+group by customername 
+order by numPayments desc;
+/*List the total payments earned by each salesperson. 
+Show the employee's name and the number of payments earned by them*/
+select concat(e.firstname,' ',e.lastname) employeeName,count(*) numPayments, sum(amount) totalEarned
+from employees e, payments p, customers c
+where c.salesrepemployeenumber = e.employeenumber and c.customernumber = p.customernumber
+group by employeeName;
+/*Without using joins, find all customers who have shipped orders 
+with a total order cost greater than $60,000.*/
+Select customername from customers where customernumber in(select
+customernumber from orders where ordernumber in (select ordernumber from orderdetails
+													group by ordernumber having
+													sum(quantityordered * priceEach) >60000));
+/*Find the email addresses for employees Tom King and Barry Jones. 
+The resulting tuples should be of the form (full name, email address). 
+You should use their names in the query itself, i.e., 
+don't find out what their IDs are and use the IDs in the query*/
+Select concat(firstname,' ',lastname) fullname, email 
+from employees where concat(firstname,' ',lastname) ='Tom King' 
+or concat(firstname,' ',lastname) ='Barry Jones';
+/*Find all orders from April, 2003. The resulting tuples should be of the form 
+(order number, order date, status). Sort the tuples from most recent to least recent. */
+Select ordernumber,orderDate,status from orders 
+where orderdate::text like '2003-04-%'
+order by orderdate desc;
+/* Find all orders by customer Mini Classics. The resulting tuples should be of the form 
+(order number, order date).*/
+Select o.ordernumber,o.orderdate from orders o where customernumber = (
+select customernumber from customers where customername = 'Mini Classics');
+/* Find the number of products in the database that have been bought by at least one foreign customer.
+Foreign customers have a country field whose value is not 'USA'. 
+The result of your query should be a single number.*/
+select count(productcode) numProducts from orderdetails where ordernumber in(
+Select ordernumber from orders where customernumber in 
+	(select customernumber from customers where country != 'USA'));
+/* Find the car product(s) in the database with the lowest MSRP. 
+All car products in the database have a product line that is either Classic Cars or Vintage Cars. 
+The result(s) of your query should be of the form (product name, product line, MSRP).
+*/
+Select productname, productline,MSRP from products where productline in ('Classic Cars','Vintage Cars')
+and msrp = (select min(msrp) from products);
+/*Find all employees in the database who have made at least 25 sales 
+(i.e. have been the salesperson for at least 25 customer orders). 
+The results of your query should be of the form 
+(employee first name, employee last name, number of sales). 
+Name the number of sales column numSales. */
+Select e.firstname,e.lastname,count(o.ordernumber) numSales
+from employees e, customers c, orders o where
+e.employeenumber = c.salesrepemployeenumber and 
+c.customernumber = o.customernumber
+group by e.firstname,e.lastname
+having count(o.ordernumber) >=25;
+/*For each customer, find the number of products from each product line that they have purchased. 
+The results of your tuple should be of the form 
+(customer name, product line name, number of products from this product line).*/
 
+Select c.customername,p.productline,count(p.productcode) from 
+customers c, orders o, orderdetails od, products p
+where c.customernumber = o.customernumber and 
+od.ordernumber = o.ordernumber and 
+p.productcode = od.productcode
+group by c.customername,p.productline
+order by c.customername;
+/*Find all instances where the same customer placed different orders exactly 7 days apart. 
+The results of your query should be of the form 
+(customer name, order number 1, order date 1, order number 2, order date 2).*/
+select c.customername,o1.orderdate, o1.ordernumber,o2.ordernumber, o2.orderdate
+from orders o1, orders o2, customers c where 
+c.customernumber = o1.customernumber and
+o1.customernumber = o2.customernumber and 
+o1.ordernumber != o2.ordernumber and 
+o1.orderdate - o2.orderdate = 7;
+/* Find all customers who have never bought a product priced more than $70. 
+The results should just include the customer name.*/
+select customername from customers where customername not in
+(select c.customername from customers c, orders o, orderdetails od
+where c.customernumber = o.customernumber and
+o.ordernumber = od.ordernumber and
+od.priceeach >= 70);
+/*Find the number of sales made by each employee, including those who have never made a sale 
+(e.g. the managers, the president, etc.). 
+The results of your query should be of the form (employee ID, number of sales made). */
+Select e.employeenumber,count(o.ordernumber) from orders o join customers c 
+on o.customernumber = c.customernumber
+right join employees e on
+c.salesrepemployeenumber = e.employeenumber
+group by e.employeenumber;
